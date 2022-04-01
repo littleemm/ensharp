38e59d4f81cbb3902f8cd9d7de4b67d99e13fd7a
Younglimmm <lilysolar@naver.com>
2022-04-01 12:48:00 +0900

    sjsjsj

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) 
+            {
+                for (int j = 1; j <= n - 1; j++) 
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= (n / 2) * i - 1; j++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
