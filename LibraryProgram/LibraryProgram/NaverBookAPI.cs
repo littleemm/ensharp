6a0f50771e674472bf14d534d458ac0c3e8bd10b
Younglimmm <lilysolar@naver.com>
2022-05-02 02:28:04 +0900

    [ADD] api 클래스 코드 수정 (원하는 데이터 선별해 출력 -> 실패)

--- LibraryProgram/LibraryProgram/NaverBookAPI.cs
+++ LibraryProgram/LibraryProgram/NaverBookAPI.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
 
 namespace LibraryProgram
 {
@@ -14,7 +18,7 @@ namespace LibraryProgram
         private static string CLIENT_SECRET = "SdGR7yh_9b";
 
 
-        public void OpenAPI()
+        public string OpenAPI()
         {
             string query = "노인과 바다"; // 검색할 문자열
             string url = "https://openapi.naver.com/v1/search/book?query=" + query; // 결과가 JSON 포맷
@@ -22,21 +26,57 @@ namespace LibraryProgram
             request.Headers.Add("X-Naver-Client-Id", CLIENT_ID); // 클라이언트 아이디
             request.Headers.Add("X-Naver-Client-Secret", CLIENT_SECRET);       // 클라이언트 시크릿
 
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                string status = response.StatusCode.ToString();
-                if (status == "OK")
-                {
-                    Stream stream = response.GetResponseStream();
-                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                    string text = reader.ReadToEnd();
-                    Console.WriteLine(text);
-                }
-                else
-                {
-                    Console.WriteLine("Error 발생=" + status);
-                }
-          
-          
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string status = response.StatusCode.ToString();
+            if (status == "OK")
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                string requestResult = reader.ReadToEnd();
+
+                return requestResult;
+
+            }
+            else
+            {
+                Console.WriteLine("Error 발생=" + status);
+                return "ERROR";
+            }
+        }
+
+        private void EditResultData()
+        {
+            string requestResult = OpenAPI();
+            requestResult = requestResult.Replace("<b>", "");
+            requestResult = requestResult.Replace("</b>", "");
+            requestResult = requestResult.Replace(",", "");
+            requestResult = requestResult.Replace("title", "  NAME   ");
+            requestResult = requestResult.Replace("price", "  PRICE  ");
+            requestResult = requestResult.Replace("author", " AUTHOR  ");
+            requestResult = requestResult.Replace("publisher", "PUBLISHER");
+            requestResult = requestResult.Replace("pubdate", " PUBDATE ");
+            requestResult = requestResult.Replace("isbn", "  ISBN   ");
+
+            var parseJson = JObject.Parse(requestResult);
+            var countsOfDisplay = Convert.ToInt32(parseJson["display"]);
+
+            List<string> resultList = new List<string>();
+            for (int i = 0; i < countsOfDisplay; i++)
+            {
+
+                var title = parseJson["items"][i]["title"].ToString();
+                var price = parseJson["items"][i]["price"].ToString();
+                var author = parseJson["items"][i]["author"].ToString();
+                var publisher = parseJson["items"][i]["publisher"].ToString();
+                var pubdate = parseJson["items"][i]["pubdate"].ToString();
+                var isbn = parseJson["items"][i]["isbn"].ToString();
+
+                resultList.Add(price.ToString());
+            }
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                Console.WriteLine(resultList[i]);
+            }
         }
     }
 }
