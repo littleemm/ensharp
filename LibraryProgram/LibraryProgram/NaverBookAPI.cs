1b1bb2271af9c79272d1eeb935a328a4e4b22075
Younglimmm <lilysolar@naver.com>
2022-05-02 01:41:48 +0900

    [FIX] api 부분 코드 수정 오류 해결

--- LibraryProgram/LibraryProgram/NaverBookAPI.cs
+++ LibraryProgram/LibraryProgram/NaverBookAPI.cs
@@ -12,17 +12,16 @@ namespace LibraryProgram
     {
         private static string CLIENT_ID = "Gy_693vcZVxD7SqwKVzr";
         private static string CLIENT_SECRET = "SdGR7yh_9b";
-        
+
+
         public void OpenAPI()
         {
             string query = "노인과 바다"; // 검색할 문자열
-            string url = "https://openapi.naver.com/v1/search/book?query" + query; // 결과가 JSON 포맷
+            string url = "https://openapi.naver.com/v1/search/book?query=" + query; // 결과가 JSON 포맷
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
             request.Headers.Add("X-Naver-Client-Id", CLIENT_ID); // 클라이언트 아이디
             request.Headers.Add("X-Naver-Client-Secret", CLIENT_SECRET);       // 클라이언트 시크릿
-            try
-            {
+
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 string status = response.StatusCode.ToString();
                 if (status == "OK")
@@ -36,14 +35,7 @@ namespace LibraryProgram
                 {
                     Console.WriteLine("Error 발생=" + status);
                 }
-            }
-            catch (WebException e)
-            {
-                if (e.Response == null)
-                {
-                    throw;
-                }
-            }
+          
           
         }
     }
