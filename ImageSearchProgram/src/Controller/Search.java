01621597affa5f8e31f4055fd7bb7573090753dd
younglimmm <lilysolar@naver.com>
2022-05-09 03:29:13 +0900

    [FIX] view 코드 frame -> panel 수정, 컨트롤러 코드 수정

--- ImageSearchProgram/src/Controller/Search.java
+++ ImageSearchProgram/src/Controller/Search.java
@@ -1,24 +1,45 @@
 package Controller;
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.event.*;
 import View.*;
 
 public class Search extends JFrame{
 	private View.Basic basic;
+	private JFrame mainFrame;
 	
 	public Search() 
 	{
+		mainFrame = new JFrame();
 		basic = new View.Basic();
 	}
 	
-	public void SearchImage() {		
+	public void SearchImage() {	
+		mainFrame.setTitle("Image Search Program");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(1000, 600);
+		mainFrame.setLocation(220,100);
+		mainFrame.setLayout(null);
 		basic.PrintSearchPage();
-		JButton b = new JButton("Action");
-		basic.searchButton.addActionListener(new ActionListener());
+		
+		mainFrame.add(basic.searchPanel);
+		basic.searchButton.addActionListener(new ButtonActionListener());
+		basic.logButton.addActionListener(new ButtonActionListener());
+		
+		mainFrame.setVisible(true);
 	}
 	
-	private class ActionListener implements ActionListener {
-		
+	private class ButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton button = (JButton)e.getSource();
+			if(button.getText().equals(" 검색 ")) {
+				
+			}
+			else if(button.getText().equals(" 당신의 모든 기록 ")) {
+				
+			}
+		}
 	}
 	
 }
--- ImageSearchProgram/src/View/Basic.java
+++ ImageSearchProgram/src/View/Basic.java
@@ -1,41 +1,42 @@
 package View;
 import javax.swing.*;
+import java.awt.*;
 
 public class Basic extends JFrame {
 	public JTextField searchField;
 	public JButton searchButton;
 	public JFrame searchFrame; 
 	public JButton logButton;
-	private JPanel panel;
+	public JPanel searchPanel;
+	private JPanel logPanel;
 	
 	public Basic() {
 		searchField = new JTextField();
 		searchButton = new JButton(" 검색 ");
 		logButton = new JButton(" 당신의 모든 기록 ");
+		searchPanel = new JPanel();
+		logPanel = new JPanel();
 	}
 	
 	public void PrintSearchPage() {
-		searchFrame = new JFrame();
-		searchFrame.setTitle("Image Search Program");
-		searchFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		searchFrame.setLocation(100,100);
+	
+		searchPanel.setSize(570, 100);
 
 		searchField.setSize(480, 50);
-		searchField.setLocation(350, 250);
-		searchFrame.add(searchField);
+		searchField.setLocation(0, 0);
+		searchPanel.add(searchField);
 		
 		searchButton.setSize(80, 40);
-		searchButton.setLocation(835, 255);
-		searchFrame.add(searchButton);
+		searchButton.setLocation(490, 5);
+		searchPanel.add(searchButton);
 		
 		logButton.setSize(150, 35);
-		logButton.setLocation(530, 330);
-		searchFrame.add(logButton);
-		
-		
-		searchFrame.add(new JPanel());
-		searchFrame.setSize(1200, 600);
-		searchFrame.setVisible(true);
+		logButton.setLocation(200, 60);
+		searchPanel.add(logButton);
+
+		searchPanel.setLayout(null);
+		searchPanel.setLocation(230, 300);
+		searchPanel.setVisible(true);
 	}
 	
 	public void PrintLogPage() {
