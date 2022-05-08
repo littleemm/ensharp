aaedd2bb5def721594cb12de2964cff4812013d9
younglimmm <lilysolar@naver.com>
2022-05-09 02:07:18 +0900

    [FIX] UI 코드 수정(JFrame 선언 등을 통해 안보이던 현상 개선)

--- ImageSearchProgram/src/Controller/Search.java
+++ ImageSearchProgram/src/Controller/Search.java
@@ -1,24 +1,17 @@
 package Controller;
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import View.*;
 
 public class Search extends JFrame{
 	private View.Basic basic;
+	
 	public Search() 
 	{
 		basic = new View.Basic();
 	}
 	
-	public void SearchImage() {
-		setTitle("ImageSearchProgram");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	public void SearchImage() {		
 		basic.PrintSearchPage();
-		
-		setSize(1200, 600);
-		setVisible(true);
 		
 	}
 	
--- ImageSearchProgram/src/View/Basic.java
+++ ImageSearchProgram/src/View/Basic.java
@@ -4,27 +4,30 @@ import javax.swing.*;
 public class Basic extends JFrame {
 	private JTextField searchField;
 	private JButton searchButton;
+	private JFrame searchFrame; 
 	private JPanel searchPanel;
 	
 	public Basic() {
 		searchField = new JTextField();
 		searchButton = new JButton(" 검색 ");
-		PrintSearchPage();
+		searchPanel = new JPanel();
 	}
 	
 	public void PrintSearchPage() {
-		getContentPane().setLayout(null);
-		searchPanel = new JPanel();
-		searchField.setSize(400, 50);
-		searchField.setLocation(400, 250);
-		searchPanel.add(searchField);
+		searchFrame = new JFrame();
+		searchFrame.setTitle("Image Search Program");
+		searchFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		searchFrame.setLocation(100,100);
+
+		searchField.setSize(480, 50);
+		searchField.setLocation(350, 250);
+		searchFrame.add(searchField);
+		searchButton.setSize(80, 40);
+		searchButton.setLocation(840, 255);
+		searchFrame.add(searchButton);
 		
-		searchButton.setSize(50,50);
-		searchButton.setLocation(810, 250);
-		searchPanel.add(searchButton);
-		
-		getContentPane().add(searchPanel);
-		setSize(1200, 600);
-		setVisible(true);
+		searchFrame.add(searchPanel);
+		searchFrame.setSize(1200, 600);
+		searchFrame.setVisible(true);
 	}
 }
