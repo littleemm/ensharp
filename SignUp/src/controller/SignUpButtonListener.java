c63c583ee33f5fb10743b2282fa132562f49ed63
younglimmm <lilysolar@naver.com>
2022-06-03 21:02:31 +0900

    [ADD] 메인 다음 화면(회원가입 화면) 위한 액션리스너 추가, 메인 프레임에 mainstartingPanel 추가

--- SignUp/src/controller/SignUpButtonListener.java
+++ SignUp/src/controller/SignUpButtonListener.java
@@ -1,5 +1,33 @@
 package controller;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class SignUpButtonListener {
-
+	
+	public SignUpButtonListener() {
+		
+	}
+	
+	public void showSignUpPage(JFrame mainFrame, JButton signupButton) { // 실제 회원가입 페이지 
+		signupButton.addActionListener(new SignUpButtonListenerInMainPage(mainFrame));
+	}
+	
+	private void moveToSignUpPage() {
+		
+	}
+	
+	private class SignUpButtonListenerInMainPage implements ActionListener {
+		private SignUpButtonListenerInMainPage(JFrame mainFrame) {
+			
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			JButton button = (JButton)e.getSource();
+			
+		}
+	}
+	
+	
+	
 }
--- SignUp/src/controller/SignUpMain.java
+++ SignUp/src/controller/SignUpMain.java
@@ -5,16 +5,19 @@ import view.MainScreen;
 
 public class SignUpMain extends JFrame {
 	private JFrame mainFrame;
+	private JPanel mainStartingPanel;
 	private MainScreen mainScreen;
 	private databaseConnection connection;
+	private SignUpButtonListener signUpButtonListener;
 	
 	public SignUpMain() {
 		mainFrame = new JFrame();
+		mainStartingPanel = new JPanel();
 		mainScreen = new MainScreen();
 		connection = new databaseConnection();
 	}
 	
-	public void showSignUp() {
+	public void showSignUp() { // signup 프로그램 처음 시작 화면 
 		mainFrame.setPreferredSize(new Dimension(1250, 700));
 		mainFrame.setMinimumSize(new Dimension(1250, 700));
 		mainFrame.setLayout(null);
@@ -26,10 +29,13 @@ public class SignUpMain extends JFrame {
 		mainScreen.showButtonPanel();
 		mainScreen.showLoginTextField();
 		
-		mainFrame.add(mainScreen.mainPanel);
-		mainFrame.add(mainScreen.buttonPanel);
-		mainFrame.add(mainScreen.loginPanel);
+		mainStartingPanel.setSize(new Dimension(1250, 700));
+		mainStartingPanel.setLayout(null);
+		mainStartingPanel.add(mainScreen.mainPanel);
+		mainStartingPanel.add(mainScreen.buttonPanel);
+		mainStartingPanel.add(mainScreen.loginPanel);
 		
+		mainFrame.add(mainStartingPanel);
 		mainFrame.repaint();
 		mainFrame.revalidate();
 		mainFrame.setVisible(true);
