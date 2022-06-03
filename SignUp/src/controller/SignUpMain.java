c832f320c686b99088e3b71f4ce7a45a82770247
younglimmm <lilysolar@naver.com>
2022-06-03 13:51:49 +0900

    [FIX] 이미지 채우기 코드 수정

--- SignUp/src/controller/SignUpMain.java
+++ SignUp/src/controller/SignUpMain.java
@@ -1,7 +1,7 @@
 package controller;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import view.*;
+import javax.swing.*;
+import java.awt.*;
+import view.MainScreen;
 
 public class SignUpMain extends JFrame {
 	private JFrame mainFrame;
@@ -15,12 +15,13 @@ public class SignUpMain extends JFrame {
 	public void showSignUp() {
 		mainFrame.setBounds(200, 200, 1000, 800);
 		mainFrame.setLayout(null);
+		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		mainScreen.showMainScreen();
-
 		
 		mainFrame.add(mainScreen.mainPanel);
+		
 		mainFrame.setVisible(true);
 	}
 	
--- SignUp/src/view/MainScreen.java
+++ SignUp/src/view/MainScreen.java
@@ -1,5 +1,28 @@
 package view;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class MainScreen {
-
+	public Panel mainPanel = new Panel();
+	private Image background;
+	
+	public MainScreen() {
+		mainPanel = new Panel();
+		background =new ImageIcon(MainScreen.class.getResource("../image/overwatch_main.png")).getImage();
+	}
+	
+	public void showMainScreen() {
+		mainPanel.setLayout(null);
+		mainPanel.setBounds(0, 0, 1000, 800);
+	}
+	
+	private class Panel extends JPanel {
+		public void paint(Graphics g) {//그리는 함수
+			super.paint(g);
+			g.drawImage(background, 0, 0, null);		
+		}
+	}
+	
+	
 }
